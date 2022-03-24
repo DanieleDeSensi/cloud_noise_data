8dc24139ec189aab33bb16de5c381db97c62c2d5
Daniele De Sensi <ddesensi@ethz.ch>
2022-03-24 10:32:25 +0100

    [ADD] Compress/decompress script.

--- compress_all.sh
+++ compress_all.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
+
+DECOMPRESS=false
+
+while getopts d flag
+do
+    case "${flag}" in
+        d) DECOMPRESS=true;;
+    esac
+done
+
 for folder in $(ls .)
 do
     if [ -d "$folder" ]; then
         pushd $folder > /dev/null
         for dt in $(ls .)
         do
-            if [ -d "$dt" ]; then
-                tar -zcvf ${dt}.tar.gz ${dt}
+            if [ "$DECOMPRESS" = true ] ; then
+                if [[ ${dt} == *.tar.gz ]]; then
+                    tar -xvf ${dt}
+                fi
+            else
+                # Compress only folders
+                if [ -d "$dt" ]; then         
+                    tar -zcvf ${dt}.tar.gz ${dt}
+                fi
             fi
         done
         popd > /dev/null
